d47aa4c9875f5f3ff80bc3283686cbdd6c13b8a1
shrmohit <161517182+shrmohit@users.noreply.github.com>
2025-02-06 10:34:17 +0530

    function componets

---
+++ reactjs_concepts/src/Components/Function_based_component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Function_based_component() {
+  return (
+    <div>
+      <h4>function Component</h4>
+    </div>
+  );
+}
+
+export default Function_based_component;
