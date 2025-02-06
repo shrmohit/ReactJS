ad40946f0156114f8fe077c929f93475ef840a3a
shrmohit <161517182+shrmohit@users.noreply.github.com>
2025-02-06 19:28:58 +0530

    css class and module

--- reactjs-concepts/notes.jsx
+++ reactjs-concepts/notes.jsx
@@ -28,3 +28,7 @@
 
 // PROPS
 // props is a way to transfer data from one component to another components
+
+// CSS Modules and Classes
+// CSS Class in Regular CSS: A CSS class that is globally scoped and can potentially conflict with other classes in the same project.
+// CSS Class in CSS Module: A CSS class that is scoped locally, preventing conflicts with other classes in the project. When you import a CSS Module in a JavaScript file, it exports an object with mappings from local names to global names, ensuring unique class names are generated automatically.
